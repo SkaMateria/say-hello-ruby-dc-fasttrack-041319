8e5b3f989247ec47bcfaa4b122a991bd5d00203c
active-unit-1076 <skamateria@gmail.com>
2019-04-13 17:35:25 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,8 +1,5 @@
 # Build your say_hello method here
-def say_hello
-  
-end
 def say_hello(name="Ruby Programmer")
    puts "Hello " + name + "!"
  end
- say_hello("Kent Beck")
+ say_hello("Ruby Programmer")
